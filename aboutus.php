d3c53104f6604b7cb7b44dd9269fb0f74fad8b5d
setie <setie008@fiu.edu>
2015-01-19 20:41:59 -0500

    Built with foundation
    First try at foundation.

---
+++ aboutus.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>CREFO | Welcome</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <link rel="stylesheet" href="stylesheets/normalize.css" />
+  <link rel="stylesheet" href="stylesheets/app.css" />
+  <link rel="icon" href="favicon.ico" type="img/x-icon" />
+  <script src="bower_components/modernizr/modernizr.js"></script>
+  <script src="js/vendor/modernizr.js"></script>
+</head>
+<body>
+
+  <div class="row">
+    <div class="large-12 columns">
+      <!--<h1>Welcome to CREFO club at FIU</h1> -->
+    </div>
+  </div>
+
+
+  <?php
+    include("../public_html/topbarinfo.php");
+    
+?>
+  <!-- End Top Bar -->
+<!-- 
+  
+    
+      <div class="row" id="doc-breadcrumbs">
+        <div class="large-12 columns">
+          <ul class="breadcrumbs">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="videos.html">Videos</a></li>
+            <li><a href="pictures.html">Pictures</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="aboutus.html">About us</a></li>
+            <li><a href="#">Features</a></li>
+            <li class="unavailable"><a href="#">Gene Splicing</a></li>
+            <li class="current"><a href="#">Cloning</a></li>
+          </ul>
+        </div>
+      </div>
+-->
+
+
+    <div class="row">
+      <div class="small-8 small-centered columns" >
+        <img src="img/CREFO_Logo_Black_Trans.png">
+      </div>
+    </div>
+    <div class="row">
+      <div class="small-12 " >
+        <hr>
+        
+        <p>
+
+          AY YO WERE THE EDGYISET  ON CAMPUS SO GET ON OUR LEVEL!!!!!!!!!!!!!
+
+          
+          SAMUEL ETIENNE
+     
+
+        </p>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <footer class="page-row">
+<div class="full-width footer-area">
+  
+        <div class="large-10 columns">
+            &copy; 2013
+        </div>
+        
+   
+   
+</div>
+</footer>
+
+  </script>
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script src="js/foundation/foundation.clearing.js"></script>
+<!--
+<script src="javascripts/vendor/jquery.js"></script>
+  <script src="javascripts/vendor/jquery.cookie.js"></script>
+
+  <script src="bower_components/foundation/js/foundation.min.js"></script>
+
+-->
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-19093680-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+</script>
+
+<script>
+$(document).foundation();
+</script>
+</body>
+</html>
